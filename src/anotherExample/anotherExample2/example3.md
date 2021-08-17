2598a91978846297c30a850ad5a50f406e60dfb7
ana446 <70743735+ana446@users.noreply.github.com>
2021-08-17 06:39:19 -0500

    function getAllStatus

--- src/anotherExample/anotherExample2/example3.md
+++ src/anotherExample/anotherExample2/example3.md
@@ -6,7 +6,6 @@ algunas estadísticas.
 ![md-links](https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
 
 - [Arreglos](https://curriculum.laboratoria.la/es/topics/javascript/04-arrays)
-- [Array - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/)
 - [Array.prototype.sort() - MDN Array.prototype.sort() - MDN Array.prototype.sort() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/sort)
 - [Array.prototype.forEach() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach)
 - [Array.prototype.map() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/map)
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const fsUtils = require('fs-utils');
-const md = require('markdown-it')({
-  html: true,
-  linkify: true,
-  typographer: true
-}); 
+const fetch = require('node-fetch');
+
+
 
 //convierte una ruta relativa a absoluta
-const isValidateAbsolute = (inputFilePath)=>{
-  return path.isAbsolute(inputFilePath) ? inputFilePath : path.resolve(inputFilePath);
+const isValidateAbsolute = (inputFilePath) => {
+  const normalizedPath = path.normalize(inputFilePath);
+  return path.isAbsolute(normalizedPath) ? normalizedPath : path.resolve(normalizedPath);
 };
 const prueba = isValidateAbsolute('src');
 // console.log(prueba);
@@ -18,14 +16,14 @@ const prueba = isValidateAbsolute('src');
 //función de recursividad para recorrer todos los directorios
 // const existInputFilePath = (inputFilePath) => fs.existsSync(inputFilePath);
 
-const getAllFiles = (inputFilePath , arrayOfFiles) => {
+const getAllFiles = (inputFilePath, arrayOfFiles) => {
   files = fs.readdirSync(inputFilePath);
   arrayOfFiles = arrayOfFiles || [];
   files.forEach((file) => {
-    if(fs.statSync(inputFilePath + '/' + file).isDirectory()){
-      arrayOfFiles = getAllFiles(inputFilePath  + '/' +  file, arrayOfFiles);
-    }else{
-      arrayOfFiles.push(path.join(inputFilePath , file));
+    if (fs.statSync(inputFilePath + '/' + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(inputFilePath + '/' + file, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(path.join(inputFilePath, file));
     }
   })
   return arrayOfFiles;
@@ -35,14 +33,14 @@ const result = getAllFiles(prueba);
 // console.log(result);
 
 //obtener todos los archivos .md
-const getAllFilesMd = (inputFilePath,arrayOfFilesMd) => {
+const getAllFilesMd = (inputFilePath, arrayOfFilesMd) => {
   arrayOfFilesMd = arrayOfFilesMd || [];
-  inputFilePath.map((file)=>{
+  inputFilePath.map((file) => {
     const filesMd = path.extname(file);
-    if(filesMd === '.md') {
+    if (filesMd === '.md') {
       arrayOfFilesMd.push(path.join(file));
     };
-    
+
   })
   return arrayOfFilesMd;
 }
@@ -55,20 +53,20 @@ const arrOffMD = getAllFilesMd(result);
 // /\[(.*)\]/)[1]/https*?:([^"')\s]+)/   Valores por defecto
 
 //Leer un archivo e imprimir en consola 
-const getAllLinksOfMd= (arrayOfFilesMd,linksOfFilesMd) =>{
+const getAllLinksOfMd = (arrayOfFilesMd, linksOfFilesMd) => {
   linksOfFilesMd = linksOfFilesMd || [];
-  
-  arrayOfFilesMd.map((fileOfMD)=>{
+
+  arrayOfFilesMd.map((fileOfMD) => {
     const allInfoMd = /\[(.*)\]\((https*?:([^"')\s]+))/mg;
-    const contentOfMd = fs.readFileSync(fileOfMD,'utf8');
+    const contentOfMd = fs.readFileSync(fileOfMD, 'utf8');
     const linksOfMd = contentOfMd.match(allInfoMd);
-    if(linksOfMd) {
-      linksOfMd.map((links)=>{
+    if (linksOfMd) {
+      linksOfMd.map((links) => {
         const onlyLinks = /https*?:([^"')\s]+)/;
-        const onlyTexts = /\[(.*)\]/ ;
+        const onlyTexts = /\[(.*)\]/;
         const onlyHrefsOfMd = links.match(onlyLinks)[0];
-        const onlyTextOfMD = links.match(onlyTexts)[1].substring(0,49);
-        const  arrayOfInfoFilesMd = {
+        const onlyTextOfMD = links.match(onlyTexts)[1].substring(0, 49);
+        const arrayOfInfoFilesMd = {
           href: onlyHrefsOfMd,
           text: onlyTextOfMD,
           file: fileOfMD,
@@ -76,8 +74,36 @@ const getAllLinksOfMd= (arrayOfFilesMd,linksOfFilesMd) =>{
         linksOfFilesMd.push(arrayOfInfoFilesMd);
       });
     };
+    
   });
   return linksOfFilesMd;
 };
-console.log(getAllLinksOfMd(arrOffMD));
-module.exports = {isValidateAbsolute , getAllFiles , getAllFilesMd};
+// const arrLinks= getAllLinksOfMd(arrOffMD)[0].href;
+const arrLinkss= getAllLinksOfMd(arrOffMD);
+// console.log(arrLinkss);
+
+const getAllStatus = (arrLinks) => {
+  const statusOfLinks = arrLinks.map((element) => 
+     fetch(element)
+    .then((res)=>{
+      element.status = res.status;
+      element.ok = res.status === 200 ? 'ok' :'failed';
+      return element;
+    }) 
+    .catch((err) => {
+      console.log(err.message);
+    })
+  )
+  
+  return statusOfLinks;
+}
+const answerOfStatus = getAllStatus(arrLinkss);
+
+Promise.all(answerOfStatus).then((allArrayOfStatus)=>{
+  console.log(allArrayOfStatus)
+} );
+
+//status
+
+
+module.exports = { isValidateAbsolute, getAllFiles, getAllFilesMd,getAllStatus };
